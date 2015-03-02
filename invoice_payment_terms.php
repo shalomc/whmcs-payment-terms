86c8663c4e95d1838a3162b5e4ddfe9b9420dfd0
shalomc <shalom@globaldots.com>
2015-03-02 13:36:59 +0200

    Check payment terms calculations for errors. Don't modify if in error.

--- invoice_payment_terms.php
+++ invoice_payment_terms.php
@@ -1,11 +1,11 @@
 <?php
- 
+
  function hook_invoice_payment_terms($vars) {
  // Setup
  $adminuser = "apiapi";
+ $responsetype = "json";  // Probably unnecessary
  $termsKey = 'customfields1'; 
-
- $responsetype = "json";  // Probably unnecessary 
+ 
  $invoiceid = $vars['invoiceid'];
  $invoicesource =$vars['source'] ;
  $creatinguser = $vars['user'] ; 
@@ -32,20 +32,26 @@
 
  // Add payment terms to due date from customfields1
  $datestr=$getinvoice["duedate"];
- $duedate=strtotime($datestr . " " . $getclientsdetails[$termsKey]);
-
- // Update invoice
- $command = "updateinvoice";
- $values["invoiceid"] = $invoiceid;
- $values["duedate"] = date('Ymd', $duedate);
- $values["responsetype"] = $responsetype ; 
- 
- $updateinvoice = localAPI($command,$values,$adminuser);
-
- $values = null;  
- unset($values);
-	
+ $errorOnTerms = false; 
+ // check for errors on payment terms. If error then don't modify
+ if ( ($duedate=strtotime($datestr . " " . $getclientsdetails[$termsKey])) === false) {
+	$errorOnTerms = true ;
  }
+
+ // Update invoice if not error 
+ if ( !($errorOnTerms) ) {
+	 $command = "updateinvoice";
+	 $values["invoiceid"] = $invoiceid;
+	 $values["duedate"] = date('Ymd', $duedate);
+	 $values["responsetype"] = $responsetype ; 
+ 
+	 $updateinvoice = localAPI($command,$values,$adminuser);
+
+ 	 $values = null;  
+ 	 unset($values);
+ }
+
+}
   
  add_hook("InvoiceCreation",99,"hook_invoice_payment_terms");
  
